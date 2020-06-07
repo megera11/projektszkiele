ae5dd924f15bf8e1d7e1a4628313f8e946c29ae5
megera11 <marekhajduczenia11@gmail.com>
2020-06-07 23:01:51 +0200

    ehhhh3

--- db/migrate/20200429182023_create_pokarmies.rb
+++ db/migrate/20200429182023_create_pokarmies.rb
@@ -3,13 +3,13 @@ class CreatePokarmies < ActiveRecord::Migration[6.0]
     create_table :pokarmies do |t|
       t.string :nazwa
       t.decimal :kalorie, precision: 4, scale: 1
-      t.decimal :bialko, precision: 3, scale: 1
-      t.decimal :weglowodany, precision: 3, scale: 1
-      t.decimal :tluszcz, precision: 3, scale: 1
+      t.decimal :bialko, precision: 4, scale: 1
+      t.decimal :weglowodany, precision: 4, scale: 1
+      t.decimal :tluszcz, precision: 4, scale: 1
 
       t.timestamps
     end
-    
+
   end
 
 
