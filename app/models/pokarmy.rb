36ffa96e4aaac8ff9ab503f9177c1984c91658d8
megera11 <marekhajduczenia11@gmail.com>
2020-06-07 22:53:10 +0200

    ehhhh22

--- app/models/pokarmy.rb
+++ app/models/pokarmy.rb
@@ -2,8 +2,8 @@ class Pokarmy < ApplicationRecord
   has_many :eat
   before_save {self.nazwa = nazwa.downcase}
   validates :nazwa, presence: true, length: {minimum:3 ,maximum: 30}
-  validates :kalorie, presence: true,  numericality: { greater_than: 0, less_than: 1000 }
-  validates :bialko, presence: true,  numericality: { greater_than: 0, less_than: 101 }
-  validates :weglowodany, presence: true,  numericality: { greater_than: 0, less_than: 101 }
-  validates :tluszcz, presence: true,  numericality: { greater_than: 0, less_than: 101 }
+  validates :kalorie, presence: true,  numericality: { greater_than_or_equal_to: 0, less_than: 1000 }
+  validates :bialko, presence: true,  numericality: { greater_than_or_equal_to: 0, less_than: 101 }
+  validates :weglowodany, presence: true,  numericality: { greater_than_or_equal_to: 0, less_than: 101 }
+  validates :tluszcz, presence: true,  numericality: { greater_than_or_equal_to: 0, less_than: 101 }
 end
