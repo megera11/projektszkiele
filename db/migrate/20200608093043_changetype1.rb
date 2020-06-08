73a18b3fec8c67e29604475b96e39ab001c75f2d
megera11 <marekhajduczenia11@gmail.com>
2020-06-08 12:05:43 +0200

    final1

--- db/migrate/20200608093043_changetype1.rb
+++ db/migrate/20200608093043_changetype1.rb
@@ -1,4 +1,4 @@
 class Changetype1 < ActiveRecord::Migration[6.0]
   def change
-   change_column :eats, :ilosc, :decimal, :precision => 5, :scale => 2
+   change_column :eats, :ilosc, :decimal, precision: 5, scale: 2
  end
