420aa17dab6fdfc1db79bd57fa93d4f486c7fe7c
megera11 <marekhajduczenia11@gmail.com>
2020-06-08 13:01:49 +0200

    final12301

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,7 @@
 #
 # Examples:
 
-    Pokarmy.create(nazwa: "jajko", kalorie: 140.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko2", kalorie: 150.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko3", kalorie: 160.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko4", kalorie: 170.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko5", kalorie: 190.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko6", kalorie: 100.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko7", kalorie: 111.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko8", kalorie: 112.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
-    Pokarmy.create(nazwa: "jajko9", kalorie: 123.0, bialko: 12.5, weglowodany: 0.6, tluszcz: 9.7)
+  
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
     AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
